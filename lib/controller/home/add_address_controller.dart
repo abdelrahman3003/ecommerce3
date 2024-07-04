a9b2b944409ac4c720d93923eb7edd0146d7524a
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-07-04 14:12:55 +0300

    add marker

--- lib/controller/home/add_address_controller.dart
+++ lib/controller/home/add_address_controller.dart
@@ -7,6 +7,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 abstract class AddAddressController extends GetxController {
   getCurrentPosition();
+  addMarker(LatLng lating);
 }
 
 class AddAddressControllerImp extends AddAddressController {
@@ -14,7 +15,7 @@ class AddAddressControllerImp extends AddAddressController {
   Position? position;
   CameraPosition? kGooglePlex;
   StatusRequest statusRequest = StatusRequest.loading;
-
+  List<Marker> markers = [];
   @override
   void onInit() {
     completerController = Completer<GoogleMapController>();
@@ -30,6 +31,13 @@ class AddAddressControllerImp extends AddAddressController {
       zoom: 14.4746,
     );
     statusRequest = StatusRequest.success;
+    update();
+  }
+
+  @override
+  addMarker(LatLng lating) {
+    markers.clear();
+    markers.add(Marker(markerId: MarkerId('1'), position: lating));
     update();
   }
 }
--- lib/view/home/widget/setting/address/add_address.dart
+++ lib/view/home/widget/setting/address/add_address.dart
@@ -2,6 +2,8 @@ import 'package:eccommerce4/controller/home/add_address_controller.dart';
 import 'package:eccommerce4/core/class/data_handilng.dart';
 import 'package:eccommerce4/core/class/data_handling_request.dart';
 import 'package:eccommerce4/core/class/statuscode.dart';
+import 'package:eccommerce4/core/constant/colors.dart';
+import 'package:eccommerce4/core/shared/widgets/buttons/onboarding_Button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,35 +15,47 @@ class AddAddressView extends StatelessWidget {
   Widget build(BuildContext context) {
     Get.put(AddAddressControllerImp());
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("adddress"),
+      appBar: AppBar(
+        title: const Text("adddress"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: GetBuilder<AddAddressControllerImp>(builder: (controller) {
+                return DataHandlingRequsetState(
+                    statusRequest: controller.statusRequest,
+                    widget: controller.kGooglePlex != null
+                        ? Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              GoogleMap(
+                                mapType: MapType.normal,
+                                markers: controller.markers.toSet(),
+                                onTap: (latlong) {
+                                  controller.addMarker(latlong);
+                                },
+                                initialCameraPosition: controller.kGooglePlex!,
+                                onMapCreated:
+                                    (GoogleMapController gcontroller) {
+                                  controller.completerController!
+                                      .complete(gcontroller);
+                                },
+                              ),
+                              Positioned(
+                                  bottom: 5,
+                                  child: OnboardingButton(
+                                    text: "Complete",
+                                    onPressed: () {},
+                                  ))
+                            ],
+                          )
+                        : const SizedBox());
+              }),
+            ),
+          ],
         ),
-        body: Container(
-          child: Column(
-            children: [
-              Expanded(
-                child:
-                    GetBuilder<AddAddressControllerImp>(builder: (controller) {
-                  return DataHandlingRequsetState(
-                      statusRequest: controller.statusRequest,
-                      widget: controller.kGooglePlex != null
-                          ? GoogleMap(
-                              mapType: MapType.normal,
-                              initialCameraPosition: controller.kGooglePlex!,
-                              onMapCreated: (GoogleMapController gcontroller) {
-                                controller.completerController!
-                                    .complete(gcontroller);
-                              },
-                            )
-                          : const SizedBox());
-                }),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ));
+      ),
+    );
   }
 }
